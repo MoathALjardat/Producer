9f15c484537d97645419069aa1c02a897e38c034
MoathALjardat <1180255@student.birzeit.edu>
2021-07-14 16:18:15 +0300

    This is Producer

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+import com.example.demo.student.models.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.reflect.Method;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Producer/controllers/ProducerController.java
@@ -0,0 +1,39 @@
+package com.example.demo.Producer.controllers;
+
+import com.example.demo.student.models.Student;
+import com.example.demo.student.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProducerController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/count")
+    public int countNumberOfStudents() throws InterruptedException {
+        Thread.sleep(2000);
+        return studentService.getNumberOfStudents();
+    }
+
+    @GetMapping("/getGpa/{id}")
+    public Student findStudentGpaById(@PathVariable int id) throws InterruptedException {
+        Thread.sleep(2000);
+        return studentService.getStudentById(id);
+    }
+
+    @GetMapping("/getGpa/max")
+    public Student getHighestMark() throws InterruptedException {
+        Thread.sleep(2000);
+        return studentService.getHighestMark();
+    }
+
+    @GetMapping("/getGpa/above50")
+    public List<Student> getStudentsThierMarkesAbove50() throws InterruptedException {
+        return studentService.getStudentsThierMarkesAbove50();
+    }
+}
